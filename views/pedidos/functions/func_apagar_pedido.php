e825f198823ab04aae74a2949c7a93dc98a8dd94
JCardilho <115733809+JCardilho@users.noreply.github.com>
2023-11-11 12:50:41 -0300

    Update func_apagar_pedido.php

--- views/pedidos/functions/func_apagar_pedido.php
+++ views/pedidos/functions/func_apagar_pedido.php
@@ -1,14 +1,12 @@
 <?php
-include("../../../utils/verificar_login.php");
-include("../../../conexao.php");
-verificar_admin();
+include("../../../common/config.php");
 
 // Use $_POST em vez de $_GET
 $id = $_POST['id_pedido'];
 
 // Use prepared statement para evitar SQL injection
 $sql_apagar_pedido = "DELETE FROM pedidos WHERE id = ?";
-$stmt = mysqli_prepare($con, $sql_apagar_pedido);
+$stmt = mysqli_prepare($conexao, $sql_apagar_pedido);
 
 // Vincule o parâmetro
 mysqli_stmt_bind_param($stmt, "i", $id);
@@ -17,12 +15,11 @@ mysqli_stmt_bind_param($stmt, "i", $id);
 if (mysqli_stmt_execute($stmt)) {
     header("Location: ../pedidos");
 } else {
-    echo "Erro: " . mysqli_error($con);
+    echo "Erro: " . mysqli_error($conexao);
 }
 
 // Feche o statement
 mysqli_stmt_close($stmt);
 
 // Feche a conexão
-mysqli_close($con);
-?>
+mysqli_close($conexao);
