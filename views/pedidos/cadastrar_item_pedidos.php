42aea0c75165cf5d964550196516bd61d862e6b1
JCardilho <115733809+JCardilho@users.noreply.github.com>
2023-11-11 13:10:25 -0300

    Update cadastrar_item_pedidos.php

--- views/pedidos/cadastrar_item_pedidos.php
+++ views/pedidos/cadastrar_item_pedidos.php
@@ -43,7 +43,7 @@ $result_itens_pedidos = mysqli_query($conexao, $sql_itens_pedidos);
     <?php
     /* exibir o inner join */
     while ($row = mysqli_fetch_array($result_itens_pedidos)) {
-        echo "<p>" . $row['nome'] . " - " . $row['qtde'] . " - " . $row['valor_unitario'] . "</p>";
+        echo "<p>" . "<a href='./functions/func_apagar_items_pedido.php?id_item=" . $row["id_item"] . "&id_pedido=". $row["id_pedido"] . "'>" . "X" . "</a>" . $row['nome'] . " - " . $row['qtde'] . " - " . $row['valor_unitario'] . "</p>";
     }
     ?>
 </div>
