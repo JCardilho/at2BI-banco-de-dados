bbd1322058efd2ce2b031d12120bc5cd7e54c5ee
JCardilho <115733809+JCardilho@users.noreply.github.com>
2023-11-11 13:10:43 -0300

    Update func_criar_items_pedidos.php

--- views/pedidos/functions/func_criar_items_pedidos.php
+++ views/pedidos/functions/func_criar_items_pedidos.php
@@ -9,7 +9,7 @@ $quantidade = $_POST['quantidade'];
 $sql_criar_item_pedido = "INSERT INTO itens_pedido (id_pedido, id_produto, qtde) VALUES ('$id_pedido', '$id_produto', '$quantidade')";
 
 if (mysqli_query($conexao, $sql_criar_item_pedido)) {
-    header("Location: ../");
+    header("Location: ../cadastrar_item_pedidos.php?id=$id_pedido");
 } else {
     echo "Erro: " . mysqli_error($con);
 }
